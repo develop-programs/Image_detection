50cda47c2338e9bede724052cc9cc9af3b789056
shreyansh awadhiya <shreyanshawadhiya@gmail.com>
2024-10-27 20:36:17 +0530

    auth pages

--- src/app/api/auth/signIn/page.tsx
+++ src/app/api/auth/signIn/page.tsx
@@ -1,9 +1,10 @@
+import LoginForm from '@/components/custom/login-form'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      
+    <div className='min-h-screen flex justify-center items-center'>
+      <LoginForm />
     </div>
   )
 }
--- src/app/api/auth/signUp/page.tsx
+++ src/app/api/auth/signUp/page.tsx
@@ -1,9 +1,10 @@
+import SignupForm from '@/components/custom/signup-form'
 import React from 'react'
 
 export default function page() {
     return (
-        <div>
-
+        <div className='min-h-screen flex justify-center items-center'>
+            <SignupForm />
         </div>
     )
 }
