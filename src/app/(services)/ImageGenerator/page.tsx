1c29f8c891b9ff0e9f24734d8f81bce9abf29273
shreyansh awadhiya <shreyanshawadhiya@gmail.com>
2024-11-04 07:31:52 +0530

    changes

--- src/app/(services)/ImageGenerator/page.tsx
+++ src/app/(services)/ImageGenerator/page.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { PlaceholdersAndVanishInput } from "@/components/ui/placehlder-vanishing";
+import Image from "next/image";
+import React from "react";
 
 export default function page() {
+  const [input, setInput] = React.useState<string>("");
+
   const placeholders = [
     "Describe a futuristic cityscape at sunset",
     "A serene beach with crystal clear water",
@@ -12,23 +16,35 @@ export default function page() {
   ];
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        setInput(event.target?.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
   };
-  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+  const onSubmit = (e: any) => {
     e.preventDefault();
-    console.log(e)
-    console.log("submitted");
+    console.log(e);
+    setInput(e.target.value);
+
   };
   return (
-    <div className="h-[40rem] flex flex-col justify-center items-center px-4">
-      <h2 className="mb-10 sm:mb-20 text-xl text-center sm:text-5xl dark:text-white text-black">
-        Generate anything you want
-      </h2>
-      <PlaceholdersAndVanishInput
-        placeholders={placeholders}
-        onChange={handleChange}
-        onSubmit={onSubmit}
-      />
+    <div>
+      <div className="h-[40rem] flex flex-col justify-center items-center px-4">
+        <h2 className="mb-10 sm:mb-20 text-xl text-center sm:text-5xl dark:text-white text-black">
+          Generate anything you want
+        </h2>
+        <PlaceholdersAndVanishInput
+          placeholders={placeholders}
+          onChange={handleChange}
+          onSubmit={onSubmit}
+        />
+      </div>
+      {input && <Image src={input} width={500} height={500} alt="Uploaded Image" />}
     </div>
   );
 }
